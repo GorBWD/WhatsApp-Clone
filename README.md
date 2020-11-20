10f3a48ee992033c80d4d42808c4eb8b2dda8500
Gor Sharmazanyan <gsharmazanyan@bluip.com>
2020-11-20 14:32:43 +0400

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Whatsapp-Clone
+```
+npm install
+```
+
+### Run Backend And Front-end
+
+```
+npm run dev
+```
---
+++ client/src/contexts/SocketProvider.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from 'react';
+import io from 'socket.io-client'
+
+const SocketContext = React.createContext()
+
+export function useSocket() {
+    return useContext(SocketContext)
+}
+
+export function SocketProvider({ id, children }) {
+    const [socket, setSocket] = useState();
+    
+    useEffect(() => {
+        // const host = 
+        // const params = 
+        
+        const newSocket = io(
+            'http://localhost:7000', 
+            { query: { id } }
+        )
+
+        setSocket(newSocket)
+
+        return () => newSocket.close();
+    }, [id]);
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
+}
